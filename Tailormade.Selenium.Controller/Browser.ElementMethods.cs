1212bfbce4f71c6e684cfb487257f7782b6dcbdd
RaoulJacobs <jara@tailormade.eu>
2018-05-13 21:01:47 +0200

    Initial Commit

---
+++ Tailormade.Selenium.Controller/Browser.ElementMethods.cs
@@ -0,0 +1,130 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tailormade.Selenium.Controller
+{
+    public partial class Browser
+    {
+        public bool ClickByLinkText(string linkText, OpenQA.Selenium.IWebElement parentElement = null, int index = 0, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var el = parentElement != null ? parentElement.FindElements(By.LinkText(linkText)) : Driver.FindElementsByLinkText(linkText);
+            if (el.Count == 0)
+            {
+                return false;
+            }
+            el.ElementAt(index).Click();
+            return IsBrowserReady(sleep, checkString);
+        }
+
+        public bool ClickByClassName(string className, OpenQA.Selenium.IWebElement parentElement = null, int index = 0, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var el = parentElement != null ? parentElement.FindElements(By.ClassName(className)) : Driver.FindElementsByClassName(className);
+            if (el.Count == 0)
+            {
+                return false;
+            }
+            el.ElementAt(index).Click();
+            return IsBrowserReady(sleep, checkString);
+        }
+        public bool ClickByName(string name, OpenQA.Selenium.IWebElement parentElement = null, int index = 0, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var el = parentElement != null ? parentElement.FindElements(By.Name(name)) : Driver.FindElementsByName(name);
+            if (el.Count == 0)
+            {
+                return false;
+            }
+            el.ElementAt(index).Click();
+            return IsBrowserReady(sleep, checkString);
+        }
+        public bool ClickButtonWithText(string text, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var el = Driver.FindElementByXPath(string.Format("//button[contains(text(),'{0}')]", text));
+            if (el == null)
+            {
+                return false;
+            }
+            el.Click();
+            return IsBrowserReady(sleep, checkString);
+        }
+        public bool ClickElementWithText(string tagName,string text, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var el = Driver.FindElementByXPath(string.Format("//{0}[contains(text(),'{1}')]", tagName, text));
+            if (el == null)
+            {
+                return false;
+            }
+            el.Click();
+            return IsBrowserReady(sleep, checkString);
+        }
+        public IWebElement WaitForElementByName(string name, OpenQA.Selenium.IWebElement parentElement = null)
+        {
+            IWebElement el = null;
+            DateTime dt = DateTime.Now.AddMilliseconds(MaxRenderWait);
+            while (DateTime.Now < dt)
+            {
+                try
+                {
+                    el = (parentElement != null) ? parentElement.FindElement(By.Name(name)) : Driver.FindElementByName(name);
+                    if (el != null)
+                    {
+                        return el;
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+                Task.Delay(100).Wait();
+            }
+            return Driver.FindElementByName(name);
+        }
+
+        public IWebElement WaitForElementByClassName(string name, OpenQA.Selenium.IWebElement parentElement = null)
+        {
+            IWebElement el = null;
+            DateTime dt = DateTime.Now.AddMilliseconds(MaxRenderWait);
+            while (DateTime.Now < dt)
+            {
+                try
+                {
+                    el = (parentElement != null) ? parentElement.FindElement(By.ClassName(name)) : Driver.FindElementByClassName(name);
+                    if (el != null)
+                    {
+                        return el;
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+                Task.Delay(100).Wait();
+            }
+            return Driver.FindElementByClassName(name);
+        }
+        public IWebElement WaitForElementById(string name, OpenQA.Selenium.IWebElement parentElement = null)
+        {
+            IWebElement el = null;
+            DateTime dt = DateTime.Now.AddMilliseconds(MaxRenderWait);
+            while (DateTime.Now < dt)
+            {
+                try
+                {
+                    el = (parentElement != null) ? parentElement.FindElement(By.Id(name)) : Driver.FindElementById(name);
+
+                    if (el != null)
+                    {
+                        return el;
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+                Task.Delay(100).Wait();
+            }
+            return Driver.FindElementById(name);
+        }
+
+    }
+}
---
+++ Tailormade.Selenium.Controller/Browser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tailormade.Selenium.Controller
+{
+    public partial class Browser : IDisposable
+    {
+        public OpenQA.Selenium.Remote.RemoteWebDriver Driver { get; set; }
+        public int DefaultSleep { get; set; }
+        public int NavigateCount { get; set; }
+        public int MaxRenderWait { get; set; }
+        public Browser()
+        {
+            this.DefaultSleep = 2000;
+            this.MaxRenderWait = 5000;
+            RecycleDriver();
+        }
+
+        public bool Navigate(string url, int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            Driver.ExecuteScript("window.focus();");
+            Driver.Navigate().GoToUrl(url);
+            IsBrowserReady(sleep: sleep, checkString: checkString);
+            NavigateCount++;
+            return (Driver.Url.IndexOf(url) > -1);
+        }
+        public bool IsBrowserReady(int sleep = 0, string checkString = "||||||||||||||||")
+        {
+            var dt = DateTime.Now.AddMilliseconds(sleep == 0 ? DefaultSleep : sleep);
+            var checkStringFound = false;
+            while (!checkStringFound && DateTime.Now < dt)
+            {
+                checkStringFound = checkString == "||||||||||||||||" ? false : (Driver.PageSource.IndexOf(checkString, StringComparison.InvariantCultureIgnoreCase) > -1);
+                if (!checkStringFound)
+                {
+                    Task.Delay(100).Wait();
+                    //System.Threading.Thread.Sleep(10);
+                }
+            }
+            //System.Threading.Thread.Sleep(sleep==0?DefaultSleep:sleep);
+            return true;
+        }
+
+        
+
+        public bool RecycleDriver(int recycleNavigateCount = 100)
+        {
+            if (this.Driver != null && this.NavigateCount < recycleNavigateCount)
+            { return false; }
+            if (this.Driver != null)
+            {
+                this.Driver.Quit();
+            }
+            var options = new OpenQA.Selenium.Chrome.ChromeOptions();
+            options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+            options.AddArguments("--start-maximized");
+            this.Driver = new OpenQA.Selenium.Chrome.ChromeDriver(options);
+            //this.RemoteWebDriver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            NavigateCount = 0;
+            return true;
+        }
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
